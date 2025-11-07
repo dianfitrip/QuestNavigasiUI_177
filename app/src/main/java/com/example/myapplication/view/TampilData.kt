1e31aaf2a18146f6710aaddc1bb5102531c89c7c
dianfitrip <dianfitri683@gmail.com>
2025-11-07 21:31:15 +0700

    tambahkan composable Formulirku dengan aksi pindah ke Detail

--- app/src/main/java/com/example/myapplication/view/TampilData.kt
+++ app/src/main/java/com/example/myapplication/view/TampilData.kt
@@ -16,7 +16,14 @@ fun DataApp(
             startDestination = Navigasi.Formulirku.name,
             modifier = Modifier.padding(paddingValues = isiRuang)
         ) {
-
+            composable(route = Navigasi.Formulirku.name) {
+                FormisPian(
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
         }
     }
 }
