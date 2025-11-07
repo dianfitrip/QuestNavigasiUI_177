dd1d1d406b4be96d3e68bea4cb637a3c228eb360
dianfitrip <dianfitri683@gmail.com>
2025-11-07 10:19:05 +0700

    tambahkan OutlinedTextField untuk input Alamat

--- app/src/main/java/com/example/myapplication/view/Formulir.kt
+++ app/src/main/java/com/example/myapplication/view/Formulir.kt
@@ -51,6 +51,14 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modfier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange ={},
+            )
         }
     }
 }
