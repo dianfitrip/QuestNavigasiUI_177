1509540606fcbd9cb4a0c533b261fe97b1bee862
dianfitrip <dianfitri683@gmail.com>
2025-11-07 21:29:28 +0700

    tambahkan NavHost ke dalam Scaffold dengan startDestination Formulirku

--- app/src/main/java/com/example/myapplication/view/TampilData.kt
+++ app/src/main/java/com/example/myapplication/view/TampilData.kt
@@ -5,3 +5,18 @@ enum class Navigasi {
     Formulirku,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+
+        }
+    }
+}
